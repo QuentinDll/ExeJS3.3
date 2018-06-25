9e919f2e85d66e4a6e1fe720caa5c408444192cc
QuentinDll <quentin.delplace13@gmail.com>
2018-06-25 15:51:04 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function changeGreen(){
+  document.getElementById('text').style.color = 'green';
+}
+function changeRed(){
+  document.getElementById('text').style.color = 'red';
+}
+function changeBlue(){
+  document.getElementById('text').style.color = 'blue';
+}
